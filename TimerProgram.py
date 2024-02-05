89003ec096c40fb8676ecbc01fa747e807a4d61b
Ramez Halasah <ramezhalasah@gmail.com>
2024-02-04 20:30:39 -0700

    Just some improvements

--- TimerProgram.py
+++ TimerProgram.py
@@ -7,6 +7,7 @@
 import time         # importing the time library, I think I need this for the actual timer
 from tkinter import *
 from tkinter import ttk
+import winsound
 
 
 
@@ -29,12 +30,26 @@ def countdown(input):
         time.sleep(1)
         #Do action here for every seconds
         seconds_left -= 1
-    
+    winsound.MessageBeep(winsound.MB_ICONEXCLAMATION)
     root = Tk()
-    frm = ttk.Frame(root, padding=10)
+    root.geometry("1500x1500")
+    frm = ttk.Frame(root,width=1500,height=1500, padding=10)
     frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+    ttk.Label(frm, text="DONE").grid(column=0, row=0)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+    
+    # Make window appear above all other windows
+    root.attributes('-topmost', True)
+    root.update()
+    root.attributes('-topmost', False)
+
+    # Additionally, use lift and focus_force to bring the window to the front and focus
+    root.lift()
+    root.focus_force()
+    
+    
+    
+    
     root.mainloop()
     print("Done")
     
